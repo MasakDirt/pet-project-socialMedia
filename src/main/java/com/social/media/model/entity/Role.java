41356c92ddc28f214a5a3c9c3d8ffdd1382f1a68
maksym_korniev <maksimkarulet8@gmail.com>
2023-07-19 00:00:40 +0300

    create User and Role entities

---
+++ src/main/java/com/social/media/model/entity/Role.java
@@ -0,0 +1,55 @@
+package com.social.media.model.entity;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.List;
+import java.util.Objects;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
+public class Role implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank(message = "The 'name' cannot be empty")
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @JsonManagedReference
+    @OneToMany(mappedBy = "role", cascade = CascadeType.ALL)
+    List<User> users;
+
+    @Override
+    public String getAuthority() {
+        return "ROLE_" + this.name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return id == role.id && Objects.equals(name, role.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/social/media/model/entity/User.java
@@ -0,0 +1,82 @@
+package com.social.media.model.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+    private static final String NAME_REGEXP = "[A-Z][a-z]+(-[A-Z][a-z]+){0,1}";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank(message = "The 'username' cannot be empty")
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    @NotBlank(message = "The 'first name' cannot be empty")
+    @Pattern(regexp = NAME_REGEXP,
+            message = "First name must start with a capital letter and followed by one or more lowercase")
+    private String firstName;
+
+    @Column(nullable = false)
+    @NotBlank(message = "The 'last name' cannot be empty")
+    @Pattern(regexp = NAME_REGEXP,
+            message = "Last name must start with a capital letter and followed by one or more lowercase")
+    private String lastName;
+
+    @NotBlank(message = "The 'email' cannot be empty")
+    @Pattern(regexp = "[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}", message = "Must be a valid e-mail address")
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @NotBlank(message = "The 'password' cannot be blank!")
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JsonBackReference
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && Objects.equals(username, user.username) && Objects.equals(firstName, user.firstName)
+                && Objects.equals(lastName, user.lastName) && Objects.equals(email, user.email) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, firstName, lastName, email, password);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public String getName(){
+        return firstName + " " + lastName;
+    }
+}
