d58cc70f930149067f685e40024430654c49f8ed
maksym_korniev <maksimkarulet8@gmail.com>
2023-07-19 13:14:30 +0300

    added Messenger class where we have fields: id,owner, recipient and list  messages which has amount of messages between owner and recipient users!

---
+++ src/main/java/com/social/media/model/entity/Messenger.java
@@ -0,0 +1,56 @@
+package com.social.media.model.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.Objects;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "messengers")
+public class Messenger {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @JsonBackReference
+    @JoinColumn(name = "owner_id")
+    @OneToOne(fetch = FetchType.EAGER)
+    private User owner;
+
+    @JsonBackReference
+    @JoinColumn(name = "recipient_id")
+    @ManyToOne(fetch = FetchType.EAGER)
+    private User recipient;
+
+    @JsonManagedReference
+    @OneToMany(mappedBy = "messenger", cascade = CascadeType.ALL)
+    private List<Message> messages;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Messenger messenger = (Messenger) o;
+        return id == messenger.id && Objects.equals(owner, messenger.owner) && Objects.equals(recipient, messenger.recipient);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, owner, recipient);
+    }
+
+    @Override
+    public String toString() {
+        return "Messenger{" +
+                "id=" + id +
+                ", owner=" + owner +
+                ", recipient=" + recipient +
+                '}';
+    }
+}
