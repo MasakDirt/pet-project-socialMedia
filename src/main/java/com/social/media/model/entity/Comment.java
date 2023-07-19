9ab364bb0135aa1f74a8eee74034500df9c6a1ac
maksym_korniev <maksimkarulet8@gmail.com>
2023-07-19 12:33:03 +0300

    added Comment class  where we have fields: id, timestamp, comment,owner and post where we write our comment

---
+++ src/main/java/com/social/media/model/entity/Comment.java
@@ -0,0 +1,64 @@
+package com.social.media.model.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String comment;
+
+    @NotNull
+    @CreationTimestamp
+    private LocalDateTime timestamp;
+
+    @JsonBackReference
+    @JoinColumn(name = "owner_id")
+    @ManyToOne(fetch = FetchType.EAGER)
+    private User owner;
+
+    @JsonBackReference
+    @JoinColumn(name = "post_id")
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Post post;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Comment commentObj = (Comment) o;
+        return id == commentObj.id && Objects.equals(comment, commentObj.comment) &&
+                Objects.equals(timestamp.toLocalTime(), commentObj.timestamp.toLocalTime()) && Objects.equals(owner, commentObj.owner);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, comment, timestamp, owner);
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "id=" + id +
+                ", comment='" + comment + '\'' +
+                ", timestamp=" + timestamp +
+                ", owner=" + owner +
+                '}';
+    }
+}
