5777277bb059d868fbe99b02d28c9221d98d6d28
maksym_korniev <maksimkarulet8@gmail.com>
2023-07-19 13:13:45 +0300

    added Message class where we have fields: id,message, timestamp and messenger which has owner and recipient User!

---
+++ src/main/java/com/social/media/model/entity/Message.java
@@ -0,0 +1,58 @@
+package com.social.media.model.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "messages")
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank
+    @Column(nullable = false, length = 500)
+    private String message;
+
+    @NotNull
+    @CreationTimestamp
+    private LocalDateTime timestamp;
+
+    @JsonBackReference
+    @Column(name = "messenger_id")
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Messenger messenger;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Message messageObj = (Message) o;
+        return id == messageObj.id && Objects.equals(message, messageObj.message) &&
+                Objects.equals(timestamp.toLocalTime(), messageObj.timestamp.toLocalTime()) && Objects.equals(messenger, messageObj.messenger);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, message, messenger);
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "id=" + id +
+                ", message='" + message + '\'' +
+                ", messenger=" + messenger +
+                '}';
+    }
+}
