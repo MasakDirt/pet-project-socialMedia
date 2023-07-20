38705bc510271659582a524682377d098a89077b
maksym_korniev <maksimkarulet8@gmail.com>
2023-07-20 22:51:15 +0300

    added to LikeRepository method findByOwnerAndPost and created new class LikeService

--- src/main/java/com/social/media/repository/LikeRepository.java
+++ src/main/java/com/social/media/repository/LikeRepository.java
@@ -1,8 +1,14 @@
 package com.social.media.repository;
 
 import com.social.media.model.entity.Like;
+import com.social.media.model.entity.Post;
+import com.social.media.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
 @Repository
 public interface LikeRepository extends JpaRepository<Like, Long> {
+    Like findByOwnerAndPost(User owner, Post post);
 }
