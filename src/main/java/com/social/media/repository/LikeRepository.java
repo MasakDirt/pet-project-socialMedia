803274bfca645e8108115fb34ef886c8cd80b5df
maksym_korniev <maksimkarulet8@gmail.com>
2023-07-20 22:54:13 +0300

    remove unnecessary import

--- src/main/java/com/social/media/repository/LikeRepository.java
+++ src/main/java/com/social/media/repository/LikeRepository.java
@@ -6,8 +6,6 @@ import com.social.media.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface LikeRepository extends JpaRepository<Like, Long> {
     Like findByOwnerAndPost(User owner, Post post);
