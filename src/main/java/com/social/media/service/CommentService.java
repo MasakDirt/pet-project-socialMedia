850e66130fe5e88ae7452e107ca31177afd471d7
maksym_korniev <maksimkarulet8@gmail.com>
2023-07-22 13:50:06 +0300

    refactor CommentService class: changed create, update, and added getAll methods

--- src/main/java/com/social/media/service/CommentService.java
+++ src/main/java/com/social/media/service/CommentService.java
@@ -1,10 +1,14 @@
 package com.social.media.service;
 
+import com.social.media.exception.InvalidTextException;
 import com.social.media.model.entity.Comment;
 import com.social.media.repository.CommentRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 @AllArgsConstructor
@@ -13,24 +17,43 @@ public class CommentService {
     private final UserService userService;
     private final PostService postService;
 
-    public Comment create(long ownerId, long postId, Comment comment) {
-        comment.setOwner(userService.readById(ownerId));
-        comment.setPost(postService.readById(postId));
+    private static final String COMMENT_MESSAGE_FOR_EXC = "Comment need to be filled";
 
-        return commentRepository.save(comment);
+    public Comment create(long ownerId, long postId, String comment) {
+
+        if (comment == null || comment.trim().isEmpty()) {
+            throw new InvalidTextException(COMMENT_MESSAGE_FOR_EXC);
+        }
+
+        Comment commentObj = new Comment();
+        commentObj.setComment(comment);
+        commentObj.setOwner(userService.readById(ownerId));
+        commentObj.setPost(postService.readById(postId));
+
+        return commentRepository.save(commentObj);
     }
 
-    public Comment readById(long id){
+    public Comment readById(long id) {
         return commentRepository.findById(id).orElseThrow(() ->
                 new EntityNotFoundException("Comment with id" + id + "not found"));
     }
 
-    public Comment update(Comment updatedComment) {
-        readById(updatedComment.getId());
-       return commentRepository.save(updatedComment);
+    public Comment update(long id, String updatedComment) {
+        if (updatedComment == null || updatedComment.trim().isEmpty()) {
+            throw new InvalidTextException(COMMENT_MESSAGE_FOR_EXC);
+        }
+
+        var oldComment = readById(id);
+        oldComment.setComment(updatedComment);
+
+        return commentRepository.save(oldComment);
     }
 
-    public void delete(long id){
+    public void delete(long id) {
         commentRepository.delete(readById(id));
+    }
+
+    public Set<Comment> getAll() {
+        return new HashSet<>(commentRepository.findAll());
     }
 }
