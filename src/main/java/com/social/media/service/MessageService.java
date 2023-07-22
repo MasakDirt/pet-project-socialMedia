a686011de6edaa1009147d5084854582f3debd22
maksym_korniev <maksimkarulet8@gmail.com>
2023-07-23 00:39:46 +0300

    MessageService create reviewing a message that comes to methods create and update, create method getAll messages and create readByMessenger method

--- src/main/java/com/social/media/service/MessageService.java
+++ src/main/java/com/social/media/service/MessageService.java
@@ -1,10 +1,15 @@
 package com.social.media.service;
 
+import com.social.media.exception.InvalidTextException;
 import com.social.media.model.entity.Message;
 import com.social.media.repository.MessageRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Service
 @AllArgsConstructor
@@ -13,6 +18,8 @@ public class MessageService {
     private final MessengerService messengerService;
 
     public Message create(long messengerId, String message) {
+        checkValidMessage(message);
+
         var messageObj = new Message();
         messageObj.setMessage(message);
         messageObj.setMessenger(messengerService.readById(messengerId));
@@ -25,12 +32,31 @@ public class MessageService {
                 new EntityNotFoundException("Message with id " + id + " not found!"));
     }
 
-    public Message update(Message updatedMessage){
-        readById(updatedMessage.getId());
-        return messageRepository.save(updatedMessage);
+    public Message update(long messageId, String updatedMessage) {
+        checkValidMessage(updatedMessage);
+
+        var oldMessage = readById(messageId);
+        oldMessage.setMessage(updatedMessage);
+
+        return messageRepository.save(oldMessage);
     }
 
     public void delete(long id) {
         messageRepository.delete(readById(id));
+    }
+
+    public Set<Message> getAll() {
+        return new HashSet<>(messageRepository.findAll());
+    }
+
+    public List<Message> readAllByMessenger(long messengerId) {
+        var messenger = messengerService.readById(messengerId);
+        return messageRepository.findAllByMessenger(messenger);
+    }
+
+    private void checkValidMessage(String message) throws InvalidTextException {
+        if (message == null || message.trim().isEmpty()) {
+            throw new InvalidTextException("The message must contain the text!");
+        }
     }
 }
