8cf95e501b4491e72026fbbc90084da5c61fc1ae
maksym_korniev <maksimkarulet8@gmail.com>
2023-07-23 13:43:42 +0300

    MessengerService class refactor created methods, added getAll method + createNewMessengerId

--- src/main/java/com/social/media/service/MessengerService.java
+++ src/main/java/com/social/media/service/MessengerService.java
@@ -1,10 +1,15 @@
 package com.social.media.service;
 
+import com.social.media.exception.InvalidTextException;
+import com.social.media.exception.SameUsersException;
 import com.social.media.model.entity.Messenger;
 import com.social.media.repository.MessengerRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 @AllArgsConstructor
@@ -12,27 +17,40 @@ public class MessengerService {
     private final MessengerRepository messengerRepository;
     private final UserService userService;
 
-    public Messenger create(long ownerId, long recipientId) {
-        var messengerForOwner = new Messenger();
-        messengerForOwner.setOwner(userService.readById(ownerId));
-        messengerForOwner.setRecipient(userService.readById(recipientId));
+    private static final SameUsersException SAME_USERS_EXCEPTION = new SameUsersException("You cannot create messenger for you, so write please, another user!");
 
-        var messengerForRecipient = new Messenger();
-        messengerForRecipient.setRecipient(userService.readById(ownerId));
-        messengerForRecipient.setOwner(userService.readById(recipientId));
+    public Messenger create(long ownerId, long recipientId) {
+        if (ownerId == recipientId){
+            throw SAME_USERS_EXCEPTION;
+        }
+
+        var messengerForOwner = createNewMessengerId(ownerId, recipientId);
+
+        var messengerForRecipient = createNewMessengerId(recipientId, ownerId);
 
         messengerRepository.save(messengerForRecipient);
         return messengerRepository.save(messengerForOwner);
     }
 
     public Messenger create(long ownerId, String recipientUsername) {
+        if (recipientUsername == null || recipientUsername.trim().isEmpty()) {
+            throw new InvalidTextException("Username must contains at least one letter!");
+        }
+
+        var owner = userService.readById(ownerId);
+        var recipient = userService.readByUsername(recipientUsername);
+
+        if (owner.equals(recipient)){
+            throw SAME_USERS_EXCEPTION;
+        }
+
         var messengerForOwner = new Messenger();
-        messengerForOwner.setOwner(userService.readById(ownerId));
-        messengerForOwner.setRecipient(userService.readByUsername(recipientUsername));
+        messengerForOwner.setOwner(owner);
+        messengerForOwner.setRecipient(recipient);
 
         var messengerForRecipient = new Messenger();
-        messengerForRecipient.setRecipient(userService.readById(ownerId));
-        messengerForRecipient.setOwner(userService.readByUsername(recipientUsername));
+        messengerForRecipient.setOwner(recipient);
+        messengerForRecipient.setRecipient(owner);
 
         messengerRepository.save(messengerForRecipient);
         return messengerRepository.save(messengerForOwner);
@@ -47,11 +65,22 @@ public class MessengerService {
         var owner = userService.readById(ownerId);
         var recipient = userService.readById(recipientId);
 
-        return messengerRepository.findByOwnerAndRecipient(owner, recipient).orElseThrow(() ->
-                new EntityNotFoundException("Messenger for '" + owner.getName() + "' not found"));
+        return messengerRepository.findByOwnerAndRecipient(owner, recipient);
+    }
+
+    public Set<Messenger> getAll() {
+        return new HashSet<>(messengerRepository.findAll());
     }
 
     public void delete(long id) {
         messengerRepository.delete(readById(id));
+    }
+
+    private Messenger createNewMessengerId(long ownerId, long recipientId) {
+        var messenger = new Messenger();
+        messenger.setOwner(userService.readById(ownerId));
+        messenger.setRecipient(userService.readById(recipientId));
+
+        return messenger;
     }
 }
