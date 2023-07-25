d5b3f917c8ff442283fe47054e8fe78e8d2cafd0
maksym_korniev <maksimkarulet8@gmail.com>
2023-07-25 20:46:47 +0300

    refactor User class: implements UserDetails interface and it`s methods

--- src/main/java/com/social/media/model/entity/User.java
+++ src/main/java/com/social/media/model/entity/User.java
@@ -8,7 +8,11 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
@@ -16,7 +20,7 @@ import java.util.Set;
 @Setter
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
     private static final String NAME_REGEXP = "[A-Z][a-z]+(-[A-Z][a-z]+){0,1}";
 
     @Id
@@ -42,7 +46,7 @@ public class User {
 
     @NotNull
     @Pattern(regexp = "[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}", message = "Must be a valid e-mail address")
-    @Column(name = "email", nullable = false, unique = true)
+    @Column(name = "e-mail", nullable = false, unique = true)
     private String email;
 
     @NotBlank(message = "The 'password' cannot be blank!")
@@ -97,6 +101,32 @@ public class User {
                 ", lastName='" + lastName + '\'' +
                 ", email='" + email + '\'' +
                 '}';
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(role);
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     public String getName() {
