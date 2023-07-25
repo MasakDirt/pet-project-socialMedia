f50ddb039eb2168d0f5a603c757125f8a560a767
maksym_korniev <maksimkarulet8@gmail.com>
2023-07-25 22:08:10 +0300

    remove unused import

--- src/main/java/com/social/media/config/SecurityConfig.java
+++ src/main/java/com/social/media/config/SecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
