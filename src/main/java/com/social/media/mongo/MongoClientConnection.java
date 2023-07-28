fe000996a5a651a1ab7f64526b9235ac0b02d216
maksym_korniev <maksimkarulet8@gmail.com>
2023-07-28 21:44:32 +0300

    MongoClientConnection refactor method connectToMongoDb, added deleting of all entities which collection contain

--- src/main/java/com/social/media/mongo/MongoClientConnection.java
+++ src/main/java/com/social/media/mongo/MongoClientConnection.java
@@ -7,6 +7,7 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.social.media.model.entity.Message;
 import lombok.extern.slf4j.Slf4j;
+import org.bson.Document;
 
 @Slf4j
 public class MongoClientConnection {
@@ -15,6 +16,8 @@ public class MongoClientConnection {
             MongoDatabase database = mongoClient.getDatabase("messages");
             MongoCollection<Message> messageMongoCollection = database.getCollection("messages", Message.class);
 
+            // it`s for having all-time limited number of objects, and not added already existing objects which have differences only with an id.
+            messageMongoCollection.deleteMany(new Document());
             log.info("Pinged your deployment. You successfully connected to MongoDB!");
         } catch (MongoException e) {
             log.error("Error connecting to MongoDB: {}", e.getMessage(), e);
