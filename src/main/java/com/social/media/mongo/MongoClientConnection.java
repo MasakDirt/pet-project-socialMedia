3a9ceb13dd0b2be541a45df1b14f04f35d59b9a1
maksym_korniev <maksimkarulet8@gmail.com>
2023-07-28 14:47:53 +0300

    created new class mongo client connection - to connect to mongo db with url!

---
+++ src/main/java/com/social/media/mongo/MongoClientConnection.java
@@ -0,0 +1,23 @@
+package com.social.media.mongo;
+
+import com.mongodb.*;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.social.media.model.entity.Message;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MongoClientConnection {
+    public static void connectToMongoDb(String connectionString) {
+        try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+            MongoDatabase database = mongoClient.getDatabase("messages");
+            MongoCollection<Message> messageMongoCollection = database.getCollection("messages", Message.class);
+
+            log.info("Pinged your deployment. You successfully connected to MongoDB!");
+        } catch (MongoException e) {
+            log.error("Error connecting to MongoDB: {}", e.getMessage(), e);
+        }
+    }
+}
