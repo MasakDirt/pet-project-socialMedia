1e959df6f5269cd456289d6bd3352d7dd1b73a97
maksym_korniev <maksimkarulet8@gmail.com>
2023-07-30 19:22:52 +0300

    AuthEntryPointJwt refactor error that sends and added log with Content-Type

--- src/main/java/com/social/media/component/AuthEntryPointJwt.java
+++ src/main/java/com/social/media/component/AuthEntryPointJwt.java
@@ -2,16 +2,20 @@ package com.social.media.component;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Slf4j
 @Component
-public class AuthEntryPointJwt implements AuthenticationEntryPoint{
+public class AuthEntryPointJwt implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED,"Unauthorized (" + authException.getMessage() + ")");
+        log.info("Content-Type: {}", request.getContentType());
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized (please authorize before going to this URL," +
+                " go to '/api/auth/login/(username or email)' and authorize there, than return!)");
     }
 }
