a756a5911b561ecb548108b530e7870ca01f6309
maksym_korniev <maksimkarulet8@gmail.com>
2023-07-31 22:36:44 +0300

    UserMapper created methods for refactor user with difference requests

--- src/main/java/com/social/media/model/mapper/UserMapper.java
+++ src/main/java/com/social/media/model/mapper/UserMapper.java
@@ -1,7 +1,6 @@
 package com.social.media.model.mapper;
 
-import com.social.media.model.dto.user.UserCreateRequest;
-import com.social.media.model.dto.user.UserResponse;
+import com.social.media.model.dto.user.*;
 import com.social.media.model.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -10,6 +9,18 @@ import org.mapstruct.Mapping;
 public interface UserMapper {
     User createUserFromUserCreateRequest(UserCreateRequest createRequest);
 
+    @Mapping(target = "role", ignore = true)
+    User createUserFromUserCreateRequestWithRole(UserCreateRequestWithRole requestWithRole);
+
     @Mapping(target = "role", expression = "java(user.getRole().getName())")
     UserResponse createUserResponseFromUser(User user);
+
+    @Mapping(target = "password", source = "newPassword")
+    User createUserFromUserUpdateRequestById(UserUpdateRequest requestById);
+
+    @Mapping(target = "password", source = "newPassword")
+    User createUserFromUserUpdateRequestByUsername(UserUpdateRequest requestByUsername);
+
+    @Mapping(target = "password", source = "newPassword")
+    User createUserFromUserUpdateRequestByEmail(UserUpdateRequest requestByEmail);
 }
