aaf9b3c6df53dcf23b87193c34b701a684dbec93
maksym_korniev <maksimkarulet8@gmail.com>
2023-08-01 21:20:44 +0300

    added mapper for likes

---
+++ src/main/java/com/social/media/model/mapper/LikeMapper.java
@@ -0,0 +1,19 @@
+package com.social.media.model.mapper;
+
+import com.social.media.model.dto.like.LikeResponseForOwner;
+import com.social.media.model.dto.like.LikeResponseForPosts;
+import com.social.media.model.entity.Like;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface LikeMapper {
+
+    @Mapping(target = "ownerUsername", expression = "java(like.getOwner().getUsername())")
+    LikeResponseForPosts createLikeResponseForPostsFromLike(Like like);
+
+    @Mapping(target = "postId", expression = "java(like.getPost().getId())")
+    @Mapping(target = "description", expression = "java(like.getPost().getDescription())")
+    @Mapping(target = "photos", expression = "java(like.getPost().getPhotos())")
+    LikeResponseForOwner createLikeResponseForOwnerFromLike(Like like);
+}
