0aa52aabed51250e7ab6a7909f649ec2d13d4909
maksym_korniev <maksimkarulet8@gmail.com>
2023-08-01 23:04:23 +0300

    CommentRepository created 2 methods for getting All comments by post id and owner id

--- src/main/java/com/social/media/repository/CommentRepository.java
+++ src/main/java/com/social/media/repository/CommentRepository.java
@@ -4,7 +4,11 @@ import com.social.media.model.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findAllByPostId(long postId);
 
+    List<Comment> findAllByOwnerId(long ownerId);
 }
