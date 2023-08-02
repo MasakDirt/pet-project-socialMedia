63bd157b4f55d889fc3c4cc9e6781d37e3d7769e
maksym_korniev <maksimkarulet8@gmail.com>
2023-08-02 23:03:01 +0300

    Photo repository created new method for getting all photos by postID

--- src/main/java/com/social/media/repository/PhotoRepository.java
+++ src/main/java/com/social/media/repository/PhotoRepository.java
@@ -3,5 +3,8 @@ package com.social.media.repository;
 import com.social.media.model.entity.Photo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PhotoRepository extends JpaRepository<Photo, Long> {
+    List<Photo> findAllByPostId(long postId);
 }
