d92051af384e062d570b09eb8dd8abacf499eea5
maksym_korniev <maksimkarulet8@gmail.com>
2023-08-03 18:51:15 +0300

    created new responses for messenger(all messengers and one)

--- src/main/java/com/social/media/repository/MessengerRepository.java
+++ src/main/java/com/social/media/repository/MessengerRepository.java
@@ -1,7 +1,6 @@
 package com.social.media.repository;
 
 import com.social.media.model.entity.Messenger;
-import com.social.media.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
