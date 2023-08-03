7bdf958388033d76336512206706d3667bf66feb
maksym_korniev <maksimkarulet8@gmail.com>
2023-08-03 23:08:19 +0300

    RoleRepositoryTests removed unused import

--- src/test/java/com/social/media/repository/RoleRepositoryTests.java
+++ src/test/java/com/social/media/repository/RoleRepositoryTests.java
@@ -1,7 +1,6 @@
 package com.social.media.repository;
 
 import com.social.media.model.entity.Role;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
