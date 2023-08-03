940abf339df13982b09dbd04eb8d29a4c81441b8
maksym_korniev <maksimkarulet8@gmail.com>
2023-08-03 14:32:07 +0300

    refactor PhotoService class where we find the photo in minio, downloading it and then give it to user

--- src/main/java/com/social/media/service/PhotoService.java
+++ src/main/java/com/social/media/service/PhotoService.java
@@ -1,8 +1,13 @@
 package com.social.media.service;
 
+import com.social.media.exception.ConnectionToMinIOFailed;
 import com.social.media.exception.LastPhotoException;
+import com.social.media.exception.PhotoGettingException;
+import com.social.media.exception.PostCreationException;
+import com.social.media.minio.MinioClientImpl;
 import com.social.media.model.entity.Photo;
 import com.social.media.repository.PhotoRepository;
+import io.minio.errors.MinioException;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,6 +21,7 @@ import java.util.Set;
 @AllArgsConstructor
 public class PhotoService {
     private final PhotoRepository photoRepository;
+    private final MinioClientImpl minioClient;
 
     public Photo create(Photo photo) {
         if (photo != null) {
@@ -25,8 +31,11 @@ public class PhotoService {
     }
 
     public Photo readById(long id) {
-        return photoRepository.findById(id).orElseThrow(() ->
+        var photo = photoRepository.findById(id).orElseThrow(() ->
                 new EntityNotFoundException("Photo with id " + id + "not found."));
+
+        getPhoto(photo);
+        return photo;
     }
 
     public void delete(long postId, long id) {
@@ -46,5 +55,15 @@ public class PhotoService {
 
     private int isLastPhoto(long postId) {
         return getAllByPost(postId).size();
+    }
+
+    private void getPhoto(Photo photo) {
+        try {
+            minioClient.getPhoto(photo.getPost().getOwner().getUsername(), photo.getFile().getPath().replace("\\", "/"));
+        } catch (MinioException minioException) {
+            throw new ConnectionToMinIOFailed("Connection failed: " + minioException.getMessage());
+        } catch (Exception exception) {
+            throw new PhotoGettingException("While photo is getting exception was throwing: " + exception.getMessage());
+        }
     }
 }
