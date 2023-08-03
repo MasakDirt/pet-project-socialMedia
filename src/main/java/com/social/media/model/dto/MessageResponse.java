42253c63652e634a99d7bcb24808734ae07bfcce
maksym_korniev <maksimkarulet8@gmail.com>
2023-08-03 18:52:50 +0300

    delete unused import statement

--- src/main/java/com/social/media/model/dto/MessageResponse.java
+++ src/main/java/com/social/media/model/dto/MessageResponse.java
@@ -3,7 +3,6 @@ package com.social.media.model.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 import java.time.LocalDateTime;
--- src/main/java/com/social/media/model/mapper/MessageMapper.java
+++ src/main/java/com/social/media/model/mapper/MessageMapper.java
@@ -2,9 +2,7 @@ package com.social.media.model.mapper;
 
 import com.social.media.model.dto.MessageResponse;
 import com.social.media.model.entity.Message;
-import com.social.media.service.UserService;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface MessageMapper {
