69331d3164d451979730810b34fa4a70d043b09d
maksym_korniev <maksimkarulet8@gmail.com>
2023-08-05 23:48:40 +0300

    UserService remove unused imports

--- src/main/java/com/social/media/service/UserService.java
+++ src/main/java/com/social/media/service/UserService.java
@@ -11,9 +11,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @Service
 @AllArgsConstructor
